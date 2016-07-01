77a810fa0dbc10693be08f250cf52df52d62de4b
Berkus Karchebnyy <berkus@twilio.com>
2016-07-01 09:35:08 +0300

    Add MessageTextView with nicer fonts support, add reachability indicator and default avatar

--- ip-messaging-demo-android/src/main/java/com/twilio/ipmessaging/demo/MessageViewHolder.java
+++ ip-messaging-demo-android/src/main/java/com/twilio/ipmessaging/demo/MessageViewHolder.java
@@ -29,26 +29,29 @@ import uk.co.ribot.easyadapter.annotations.ViewId;
 public class MessageViewHolder extends ItemViewHolder<MessageActivity.MessageItem>
 {
     private static int[] HORIZON_COLORS = {
-        Color.RED, Color.BLUE, Color.CYAN, Color.GREEN, Color.MAGENTA
+        Color.GRAY, Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA
     };
+
+    @ViewId(R.id.avatar)
+    ImageView imageView;
+
+    @ViewId(R.id.reachability)
+    ImageView reachabilityView;
 
     @ViewId(R.id.body)
     TextView body;
 
-    @ViewId(R.id.txtInfo)
-    TextView txtInfo;
+    @ViewId(R.id.author)
+    TextView author;
 
-    @ViewId(R.id.singleMessageContainer)
-    LinearLayout singleMessageContainer;
+    @ViewId(R.id.date)
+    TextView date;
 
-    @ViewId(R.id.consumptionHorizon)
-    LinearLayout consumptionHorizon;
+    @ViewId(R.id.consumptionHorizonIdentities)
+    RelativeLayout identities;
 
-    @ViewId(R.id.avatar_message_left)
-    ImageView imageViewLeft;
-
-    @ViewId(R.id.avatar_message_right)
-    ImageView imageViewRight;
+    @ViewId(R.id.consumptionHorizonLines)
+    LinearLayout lines;
 
     View view;
 
@@ -76,74 +79,82 @@ public class MessageViewHolder extends ItemViewHolder<MessageActivity.MessageIte
     @Override
     public void onSetValues(MessageActivity.MessageItem message, PositionInfo pos)
     {
-        StringBuffer textInfo = new StringBuffer();
         if (message != null) {
-            textInfo.append(message.getMessage().getAuthor());
-            String dateString = message.getMessage().getTimeStamp();
-            if (dateString != null) {
-                textInfo.append(":").append(dateString);
-            }
-            txtInfo.setText(textInfo.toString());
-            body.setText(message.getMessage().getMessageBody());
+            Message msg = message.getMessage();
 
-            boolean left =
-                (message.getMessage().getAuthor().equals(message.getCurrentUser())) ? true : false;
-            body.setBackgroundResource(left ? R.drawable.bubble_b : R.drawable.bubble_a);
-            singleMessageContainer.setGravity(left ? Gravity.LEFT : Gravity.RIGHT);
-            txtInfo.setGravity(left ? Gravity.LEFT : Gravity.RIGHT);
-            consumptionHorizon.removeAllViews();
-            imageViewLeft.setVisibility(View.GONE);
-            imageViewRight.setVisibility(View.GONE);
+            author.setText(msg.getAuthor());
+            body.setText(msg.getMessageBody());
+            date.setText(msg.getTimeStamp());
+
+            identities.removeAllViews();
+            lines.removeAllViews();
 
             if (message.getMembers() != null && message.getMembers().getMembers() != null) {
                 for (Member member : message.getMembers().getMembers()) {
-                    if (message.getMessage().getAuthor().equals(
-                            member.getUserInfo().getIdentity())) {
-                        if (left)
-                            fillUserAvatar(imageViewLeft, member);
-                        else
-                            fillUserAvatar(imageViewRight, member);
+                    if (msg.getAuthor().equals(member.getUserInfo().getIdentity())) {
+                        fillUserAvatar(imageView, member);
+                        fillUserReachability(reachabilityView, member);
                     }
+
                     if (member.getLastConsumedMessageIndex() != null
                         && member.getLastConsumedMessageIndex()
                                == message.getMessage().getMessageIndex()) {
-                        drawConsumptionHorizon(left, member);
+                        drawConsumptionHorizon(member);
                     }
                 }
             }
         }
     }
 
-    private void drawConsumptionHorizon(boolean left, Member member)
+    private void drawConsumptionHorizon(Member member)
     {
-        LinearLayout seenByView = new LinearLayout(getContext());
-        seenByView.setOrientation(LinearLayout.VERTICAL);
-        TextView identity = new TextView(getContext());
-        identity.setGravity(left ? Gravity.LEFT : Gravity.RIGHT);
-        identity.setLayoutParams(new LinearLayout.LayoutParams(
-            ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        String ident = member.getUserInfo().getIdentity();
+        int color = getMemberRgb(ident);
 
-        identity.setText(member.getUserInfo().getIdentity());
+        TextView identity = new TextView(getContext());
+        identity.setText(ident);
+        identity.setTextSize(8);
+        identity.setTextColor(color);
+
+        // Layout
+        final RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+        int cc = identities.getChildCount();
+        if (cc > 0) {
+            params.addRule(RelativeLayout.RIGHT_OF, identities.getChildAt(cc - 1).getId());
+        }
+        identity.setLayoutParams(params);
+
         View line = new View(getContext());
         line.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 5));
-        line.setBackgroundColor(getMemberRgb(member.getUserInfo().getIdentity()));
-        identity.setTextColor(getMemberRgb(member.getUserInfo().getIdentity()));
-        identity.setTextSize(8);
-        seenByView.addView(identity);
-        seenByView.addView(line);
-        consumptionHorizon.addView(seenByView);
+        line.setBackgroundColor(color);
+
+        identities.addView(identity);
+        lines.addView(line);
     }
 
     private void fillUserAvatar(ImageView avatarView, Member member)
     {
         JSONObject attributes = member.getUserInfo().getAttributes();
         String     avatar = (String)attributes.opt("avatar");
-        avatarView.setVisibility(View.VISIBLE);
-        avatarView.setImageBitmap(null);
         if (avatar != null) {
             byte[] data = Base64.decode(avatar, Base64.NO_WRAP);
             Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
             avatarView.setImageBitmap(bitmap);
+        }
+        else {
+            avatarView.setImageResource(R.drawable.avatar2);
+        }
+    }
+
+    private void fillUserReachability(ImageView reachabilityView, Member member) {
+        if (!TwilioApplication.get().getBasicClient().getIpMessagingClient().isReachabilityEnabled()) {
+            reachabilityView.setImageResource(R.drawable.reachability_disabled);
+        } else if (member.getUserInfo().isOnline()) {
+            reachabilityView.setImageResource(R.drawable.reachability_online);
+        } else if (member.getUserInfo().isNotifiable()) {
+            reachabilityView.setImageResource(R.drawable.reachability_notifiable);
+        } else {
+            reachabilityView.setImageResource(R.drawable.reachability_offline);
         }
     }
 
