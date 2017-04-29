9be4fcb3f745a5582c84119112033028bbe72c05
Berkus Karchebnyy <berkus@twilio.com>
2017-04-29 18:20:18 +0300

    Remove unused keys

--- chat-demo-android/src/main/java/com/twilio/chat/demo/Constants.java
+++ chat-demo-android/src/main/java/com/twilio/chat/demo/Constants.java
@@ -5,8 +5,4 @@ public interface Constants {
     public static final String EXTRA_CHANNEL = "com.twilio.chat.Channel";
     /** Key into an Intent's extras data that contains Channel SID. */
     public static final String EXTRA_CHANNEL_SID = "C_SID";
-    /** Key into an Intent's extras data that indicates an action is requested from the user. */
-    public static final String EXTRA_ACTION = "channel.action";
-    /** Key into an Intent's extras data that indicates an action type. */
-    public static final String EXTRA_ACTION_INVITE = "channel.action.invite";
 }
