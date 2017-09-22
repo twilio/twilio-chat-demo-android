8caaaeb5d4d0ad914bdf188e860d85be8b6d5730
Berkus Karchebnyy <berkus@twilio.com>
2017-09-22 11:32:40 +0300

    Update MemberViewHolder

--- chat-demo-android/src/main/java/com/twilio/chat/demo/views/MemberViewHolder.kt
+++ chat-demo-android/src/main/java/com/twilio/chat/demo/views/MemberViewHolder.kt
@@ -1,26 +1,25 @@
 package com.twilio.chat.demo.views
 
+import android.content.Context
 import com.twilio.chat.demo.R
 import com.twilio.chat.Member
 
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import kotterknife.bindView
 import eu.inloop.simplerecycleradapter.SettableViewHolder
 
-//@LayoutId(R.layout.member_item_layout)
-class MemberViewHolder(internal var view: View) : SettableViewHolder<Member>(view) {
-    val memberIdentity: TextView by bindView(R.id.identity)
-    val memberSid: TextView by bindView(R.id.member_sid)
+class MemberViewHolder : SettableViewHolder<Member> {
+    private val memberIdentity: TextView by bindView(R.id.identity)
+//    private val memberSid: TextView by bindView(R.id.member_sid)
 
-//    override fun onSetListeners() {
-//        view.setOnClickListener {
-//            val listener = getListener(OnMemberClickListener::class.java)
-//            listener?.onMemberClicked(item)
-//        }
-//    }
+    constructor(context: Context, parent: ViewGroup)
+        : super(context, R.layout.member_item_layout, parent)
+    {}
 
     override fun setData(member: Member) {
         memberIdentity.text = member.identity
+//        memberSid.text = member.sid
     }
 }
